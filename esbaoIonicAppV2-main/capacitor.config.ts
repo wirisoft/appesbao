602c58d9c8024e98d8daa4c9ed052c8961c3268e
wirisoft <wirideveloper@gmail.com>
2024-08-08 19:33:22 -0600

    configurado hasta apk por http

--- esbaoIonicAppV2-main/capacitor.config.ts
+++ esbaoIonicAppV2-main/capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'esbao',
-  webDir: 'www'
+  webDir: 'www',
+  bundledWebRuntime: false,
+  server: {
+    cleartext: true,
+    androidScheme: "http"
+  }
 };
 
 export default config;
